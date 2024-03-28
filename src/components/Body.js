663c29129d3195b1f9b0507d5c79e66cf33307f4
Fristan Lobo <Fristan@vectorconsulting.in>
2024-03-28 15:00:50 +0530

    signin, header, signup, Login

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Browse from './Browse';
 import Login from './Login';
 
 
 const Body = () => {
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login />
+        },
+        {
+            path: "/Browse",
+            element: <Browse />
+        }
+    ])
     return (
         <div>
-            <Login />
-            <Browse />
+            <RouterProvider
+                router={appRouter}
+            />
         </div>
 
     )
